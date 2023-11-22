0d8bc1a49c8ae1fea6ff17fd23b2f666c4fd8069
Navneet Gupta <navneetguptaend@gmail.com>
2023-11-22 20:27:26 +0530

    Practicles

---
+++ ex8/src/app/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = [
+    { id: 1, name: 'Product 1', price: 10.99 },
+    { id: 2, name: 'Product 2', price: 15.99 },
+    { id: 3, name: 'Product 3', price: 7.49 },
+  ];
+}
